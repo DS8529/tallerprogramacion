e7bea59b006702dcd33ee4e1f2003c9553b63c8e
DS8529 <128010276+DS8529@users.noreply.github.com>
2024-10-01 20:24:28 -0500

    2

--- gps/src/main/java/edu/ucaldas/gps/GpsApplication.java
+++ gps/src/main/java/edu/ucaldas/gps/GpsApplication.java
@@ -13,7 +13,7 @@ public class GpsApplication {
       SpringApplication.run(GpsApplication.class, args);
     }
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public String hello(@RequestParam(value = "name", defaultValue = "Por fin terminamos") String name) {
       return String.format("Hello %s!", name);
     }
 }
